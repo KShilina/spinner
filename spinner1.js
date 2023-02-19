90d2ab41737cd9e9515531983d41352333d7c311
Ekaterina Shilina <yatakshilina@gmail.com>
2023-02-19 16:15:34 -0500

    Formatted

--- spinner1.js
+++ spinner1.js
@@ -1,42 +1,43 @@
 //\r replacing/rewriting characters in the same spot after a delay.
-process.stdout.write('hello from spinner1.js... \rheyyy\n');//=>heyyy from spinner1.js...
+process.stdout.write("hello from spinner1.js... \rheyyy\n"); //=>heyyy from spinner1.js...
 setTimeout(() => {
-  process.stdout.write('\r|   ');
+  process.stdout.write("\r|   ");
 }, 100);
 
 setTimeout(() => {
-  process.stdout.write('\r/   ');
+  process.stdout.write("\r/   ");
 }, 300);
 
 setTimeout(() => {
-  process.stdout.write('\r-   ');
+  process.stdout.write("\r-   ");
 }, 500);
 
 setTimeout(() => {
   // Need to escape the backslash since it's a special character.
-  process.stdout.write('\r \\   '); 
+  process.stdout.write("\r \\   ");
 }, 700);
 
 setTimeout(() => {
-  process.stdout.write('\r|   ');
+  process.stdout.write("\r|   ");
 }, 900);
 
 setTimeout(() => {
-  process.stdout.write('\r/   ');
+  process.stdout.write("\r/   ");
 }, 1100);
 
 setTimeout(() => {
-  process.stdout.write('\r-   ');
+  process.stdout.write("\r-   ");
 }, 1300);
 
 setTimeout(() => {
-  process.stdout.write('\r\\   ');
+  process.stdout.write("\r\\   ");
 }, 1500);
 
 setTimeout(() => {
-  process.stdout.write('\r|   ');
+  process.stdout.write("\r|   ");
 }, 1700);
 
 setTimeout(() => {
-  process.stdout.write('\n');
+  // \n stands for jumping to the next command line
+  process.stdout.write("\n");
 }, 1900);
